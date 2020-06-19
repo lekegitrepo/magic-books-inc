1ce8a48b1feef5ca90268182ec16748a39ee4d7e
lekegitrepo <lekedevrepo@gmail.com>
2020-06-19 14:48:19 -0500

    create categories constant

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+const CATEGORIES = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi'
+  ];
+
 const BooksForm = () => {
   return `<form></form>`
 }
