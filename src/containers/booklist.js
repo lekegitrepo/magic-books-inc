6e0fcaf6eb644e0d87ef7f0dc469556286f3e18a
lekegitrepo <lekedevrepo@gmail.com>
2020-06-22 16:04:55 -0500

    implement booklist component

--- src/containers/booklist.js
+++ src/containers/booklist.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from '../components/book';
 
-const BooksList = () => {
-  return `<table></table>`
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>category</th>
+      </tr>
+    </thead>
+    <tbody>
+      { books.map(book => {
+        <Book key={book.id} book={book} />
+      })}
+    </tbody>
+  </table>
+);
+
+const mapStateToProps = state => ({ books: state.books, });
+
+BooksList.propTypes = {
+  books: PropTypes.instanceOf(Array).isRequired,
 }
 
-export default BooksList;
+export default connect(mapStateToProps, null,)(BooksList);
