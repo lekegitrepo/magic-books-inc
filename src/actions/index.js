54e11da1ded89c9b41e45d0c6bff3454440629a3
lekegitrepo <lekedevrepo@gmail.com>
2020-06-30 15:52:33 -0500

    add changeFliter function for action reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,6 @@ const createBook = book => ({ type: 'CREATE_BOOK', book });
 
 const removeBook = book => ({ type: 'REMOVE_BOOK', book });
 
-export { createBook, removeBook };
+const changeFilter = filter => ({ type: 'CHANGE_FILTER', filter });
+
+export { createBook, removeBook, changeFilter };
