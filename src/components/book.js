2944bc76bf9a572058c2af6ca604533e81aad9a5
lekegitrepo <lekedevrepo@gmail.com>
2020-07-09 16:49:23 -0500

    fix the css class selector error on JSX

--- src/components/book.js
+++ src/components/book.js
@@ -16,11 +16,11 @@ const Book = ({ book: { id, title, category }, removeBook, editBook=f=>f }) => (
       </div>
     </div>
     <div className="reading-status">
-      <div class="c100 p25">
+      <div className="c100 p25">
         <span>25%</span>
-        <div class="slice">
-          <div class="bar"></div>
-          <div class="fill"></div>
+        <div className="slice">
+          <div className="bar"></div>
+          <div className="fill"></div>
         </div>
       </div>
       <h3 className="percent">9%</h3>
